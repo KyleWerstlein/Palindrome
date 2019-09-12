7d380c36358abe0ad6698a2a0c56a456594b4dcc
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-12 10:46:52 -0700

    initial upload

---
+++ palindrome.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char str[80];
+  char str2[80];
+  cout << "input your string!" << endl;
+  cin.get(str, 20);
+  cin.get();
+  return 0;
+}
