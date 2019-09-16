31a1377db42d125dadfa3eaa3e8e807610a03741
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-16 10:38:41 -0700

    reverse string

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,10 +4,20 @@
 using namespace std;
 
 int main() {
-  char str[80];
-  char str2[80];
+  char str[5];
+  char str2[5];
   cout << "input your string!" << endl;
-  cin.get(str, 20);
+  cin.get(str, 81);
   cin.get();
+  int length = strlen(str);
+  for(int i = 0; i < length; i++) {
+    if((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) {
+      str2[i] = str[(length - 1) - i];
+      cout << "test" << endl;
+      cout << "str2[0]: " << str2[0] << endl;
+    }
+  }
+  cout << str << endl;
+  cout << str2 << endl;
   return 0;
 }
