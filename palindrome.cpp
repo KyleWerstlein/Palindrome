1ffedf7468bff91a81220580c1894860c822bb3a
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-18 10:49:07 -0700

    i don't know why but it started working

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,22 +4,32 @@
 using namespace std;
 
 int main() {
-  char str[5];
-  char str2[5];
+  char str[80];
+  char str2[80];
   cout << "input your string!" << endl;
   cin.get(str, 81);
   cin.get();
   int length = strlen(str);
   int letterCount = 0;
+  int nonLetterCount = 0;
   for(int i = 0; i < length; i++) {
     if((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) {
-      str2[letterCount] = str[(length - 1) - i];
+      str2[letterCount] = str[i];
       letterCount++;
     }
+    else {
+      cout << str[i] << " is not a letter" << endl;
+      nonLetterCount++;
+    }
   }
-  char strFinal[letterCount];
-  cout << str << endl;
-  cout << str2 << endl;
-  cout << strFinal << endl;
+  length = length - nonLetterCount;
+  char strFinal[length];
+  for( int i = 0; i < length; i++) {
+    strFinal[i] = str2[length - 1 - i];
+  }
+  cout << "length " << strlen(str2) << endl;
+  cout << "str " << str << endl;
+  cout << "str2 " << str2 << endl;
+  cout << "strFinal " << strFinal << endl;
   return 0;
 }
