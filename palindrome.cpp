59049475e37e3edf94f5efb6dde6b6b7c663f978
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-20 10:01:43 -0700

    added comments and palindrome check

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 int main() {
-  char str[80];
+  char str[80]; // string can be up to be 80 characters
   char str2[80];
   cout << "input your string!" << endl;
   cin.get(str, 81);
-  cin.get();
-  int length = strlen(str);
+  cin.get(); // take input
+  int length = strlen(str); // get string length
   int letterCount = 0;
   int nonLetterCount = 0;
-  for(int i = 0; i < length; i++) {
-    if((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) {
-      str2[letterCount] = str[i];
-      letterCount++;
+  for(int i = 0; i < length; i++) {  // walk through array
+    if((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) { // if str[i] is a letter
+      str2[letterCount] = str[i]; //copy to string to str array without non-letters
+      letterCount++; // count letters 
     }
     else {
       cout << str[i] << " is not a letter" << endl;
@@ -23,12 +23,24 @@ int main() {
     }
   }
   length = length - nonLetterCount;
-  char strFinal[length];
-  for( int i = 0; i < length; i++) {
+  char strFinal[length]; // array size is equal to number of letters
+  for( int i = 0; i < length; i++) { // reverse string
     strFinal[i] = str2[length - 1 - i];
   }
+  bool isPalindrome = true;
+  for(int i = 0; i < length; i++ ) { 
+    if(str2[i] != strFinal[i]) { // check for palindrome
+      isPalindrome = false;
+    }
+  }
+  if(isPalindrome) {
+    cout << "Palindrome!" << endl;
+  }
+  else {
+    cout << "Not a palindrome!" << endl;
+  }
   int length2 = strlen(strFinal);
-  for(int i = 0; i < length2; i++) {
+  for(int i = 0; i < length2; i++) { // print reversed string
     if((strFinal[i] > 64 && strFinal[i] < 91) || (strFinal[i] > 96 && strFinal[i] < 123 )) {
       cout << strFinal[i];
     }
